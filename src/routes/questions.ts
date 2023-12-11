5f8bba79f5e7476d46284cd82a4e9ed075de8a17
Felipe Araujo <devaraujofelipe@gmail.com>
2023-12-11 15:51:01 +0100

    feat: add route to get a question by key

--- src/routes/questions.ts
+++ src/routes/questions.ts
@@ -1,4 +1,5 @@
 import { FastifyInstance } from 'fastify';
+import { z } from 'zod';
 
 import { database } from '../lib/firebase';
 
@@ -10,6 +11,38 @@ export async function questions(app: FastifyInstance) {
         const questions = await database.ref('services_backend/questions').once('value');
 
         return reply.send(questions.val());
+      } catch (error) {
+        console.error(error);
+        return reply.code(500).send('Internal server error');
+      }
+    }
+  );
+
+  app.get(
+    '/questions/:key',
+    async (request, reply) => {
+      try {
+        const paramsSchema = z.object({
+          key: z.string(),
+        });
+
+        const { key } = paramsSchema.parse(request.params);
+
+        const questionsRef = await database.ref(`services_backend/questions/${key}`).once('value');
+
+        const question = questionsRef.val();
+
+        if (!question) {
+          return reply.code(404).send('Question not found');
+        }
+
+        const nextQuestion = {
+          key,
+          title: question.title[0] || question.title_alt[0],
+          options: Object.values(question.answers),
+        };
+
+        return reply.send({ nextQuestion });
       } catch (error) {
         console.error(error);
         return reply.code(500).send('Internal server error');
